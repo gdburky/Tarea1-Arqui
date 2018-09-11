f3c800a30cdb0a5a01fce8922a42cb992f54077b
Guillermo Burky <gdburky@uc.cl>
2018-09-11 19:23:59 -0300

    first test

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+import datetime
 
 def index(request):
-    return HttpResponse(request.META.get('REMOTE_ADDR'))
+    now = datetime.datetime.now()
+    html = "<html><body> %s:%s:%s </body></html>" % (now.hour, now.minute, now.second)
+    return HttpResponse(request.META.get('REMOTE_ADDR') + html)
+    # HTTP_X_FORWARDED_FOR
